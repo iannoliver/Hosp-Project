9f5e4d00a2129d7a37b240ae2e8203040586dde0
IannOli <100435787+iannoliver@users.noreply.github.com>
2023-09-22 17:21:57 -0300

    Delete Back-hosp/Hosp directory

--- Back-hosp/Hosp/src/main/java/com/projeto/hosp/repository/MedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.hosp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.hosp.domain.Medico;
-
-public interface MedicoRepository extends JpaRepository<Medico, Integer> {
-
-}

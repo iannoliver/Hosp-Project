84baf8f2363599c3405443a8a8e7d169bfaa29a5
IANN BEZERRA DE OLIVEIRA <iann.boliveira@senacsp.edu.br>
2023-10-04 17:22:30 -0300

    Atualização Front e Back

---
+++ Front-hosp/Medicos/js/script.js
@@ -0,0 +1,17 @@
+function exibirMedicos() {
+    let saida = ""
+    fetch("http://localhost:5000/api/medico/listar")
+    .then((response)=>response.json())
+    .then((dados) => {
+        dados.map((itens,ix)=>{
+            saida += `<tr><td>${itens.idmedico}</td>
+            <td>${itens.nomemed}</td>
+            <td>${itens.cpf}</td>
+            <td>${itens.crm}</td>
+            <td>${itens.emailmed}</td>
+            <td>${itens.cargo}</td>
+            </tr>`
+        })
+        document.getElementsByTagName("tbody")[0].innerHTML = saida
+    }).catch((error)=>console.error("Erro na api "+error))
+}
